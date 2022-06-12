b52cf335875e461d98bf9bd2501dbf1725c95d70
koassumo <0770407@gmail.com>
2022-06-12 18:36:19 +0300

    rxjava without coroutine

--- app/src/main/java/com/example/astroproto/ui/apod/ListAPODViewModel.kt
+++ app/src/main/java/com/example/astroproto/ui/apod/ListAPODViewModel.kt
@@ -6,30 +6,39 @@ import androidx.lifecycle.viewModelScope
 import com.example.astroproto.ApiHolder
 import com.example.astroproto.model.entity.APODResponseDTO
 import com.example.astroproto.model.retrofit.RetrofitRepoApi
-import com.project.apod.usecases.APODUseCase
+import com.project.apod.usecases.CorAPODUseCase
 import io.reactivex.rxjava3.core.Single
 import kotlinx.coroutines.*
 
 
 class ListAPODViewModel (): ViewModel() {
 
-    private val apodUseCase: APODUseCase by lazy { APODUseCase() }
+    //private val apodUseCase: CorAPODUseCase by lazy { CorAPODUseCase() }
     val liveDataAPODVertical = MutableLiveData<List<APODResponseDTO>>()
 
     init {
-        viewModelScope.launch {
-            withContext(Dispatchers.IO) {
-                val mRxSingle: Single<List<APODResponseDTO>> =
-                    RetrofitRepoApi(ApiHolder().dataApi).getRepoListApi()
-                apodUseCase.load()
-                mRxSingle.subscribe({
-                    liveDataAPODVertical.value = it.reversed()
-                }, {
-                })
-                return@withContext
-            }
-        }
+        val mRxSingle: Single<List<APODResponseDTO>> =
+            RetrofitRepoApi(ApiHolder().dataApi).getRepoListApi()
+        mRxSingle.subscribe({
+            liveDataAPODVertical.value = it.reversed()
+        }, {
+        })
+
+
+//        viewModelScope.launch {
+//            withContext(Dispatchers.IO) {
+//                val mRxSingle: Single<List<APODResponseDTO>> =
+//                    RetrofitRepoApi(ApiHolder().dataApi).getRepoListApi()
+//                //apodUseCase.load()
+//                mRxSingle.subscribe({
+//                    liveDataAPODVertical.value = it.reversed()
+//                }, {
+//                })
+//                return@withContext
+//            }
+//        }
     }
+
 
 
 //    private fun updateListFirst() {
